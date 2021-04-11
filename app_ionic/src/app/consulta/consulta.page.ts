bf7ff2f4360a3da0d53e2ca9ab6161b33d7d6f78
magefra9 <magefra9@hotmail.com>
2021-04-11 13:38:10 -0500

    Modicando los inputs de consultar

--- app_ionic/src/app/consulta/consulta.page.ts
+++ app_ionic/src/app/consulta/consulta.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ApiSwaggerService } from '../services/api-swagger.service';
 
 @Component({
@@ -10,6 +10,16 @@ export class ConsultaPage implements OnInit {
   public parte1: string = '';
   public parte2: string = '';
   public parte3: string = '';
+
+
+  values:any=[];
+
+  contador : number = 1;
+
+
+  @ViewChild('field1') field1;
+  @ViewChild('field2') field2;
+  @ViewChild('field3') field3;
 
   constructor() {}
 
@@ -29,6 +39,57 @@ export class ConsultaPage implements OnInit {
     } else {
       return ' ';
     }
+  }
+
+
+  onKeyUp(event,index){
+
+    if(event.target.value.length === 0){
+      this.setFocus(index-2);
+    }
+    if(event.target.name === "field1" && event.target.value.length===4){
+      this.setFocus(index);
+    }
+    if(event.target.name === "field2" && event.target.value.length===2){
+      this.setFocus(index);
+    }
+    if(event.target.name === "field3" && event.target.value.length===2){
+      return;
+    }
+    else{
+
+    }
+
+    event.stopPropagation();
 
   }
-}
+
+
+
+
+
+
+  setFocus(index){
+
+    switch(index){
+      case 0:
+      this.field1.setFocus();
+      break;
+      case 1:
+      this.field2.setFocus();
+      break;
+      case 2:
+      this.field3.setFocus();
+      break;
+      case 3:
+      break;
+      default:
+        break;
+      }
+ }
+
+
+  }
+
+
+
