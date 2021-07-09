04c87ab02d662d149d32118899d7db4148a4a347
magefra9 <magefra9@hotmail.com>
2021-07-09 18:14:48 -0500

    appBack
    Eliminando la appBack

--- app_back/NicosApp/NicosApp.Core/Interfaces/Repositorios/INicoRepositorio.cs
+++
@@ -1,9 +0,0 @@
-﻿using NicosApp.Core.Entidades;
-using NicosApp.Core.Interfaces.Repositorios.Base;
-
-namespace NicosApp.Core.Interfaces.Repositorios
-{
-    public interface INicoRepositorio : IRepository<Nico>
-    {
-    }
-}
