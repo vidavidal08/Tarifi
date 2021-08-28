65bddf0d0b64da9c69e578f44b4e92c83f3d4444
magefra9 <magefra9@hotmail.com>
2021-08-28 10:33:45 -0500

    Conflctos

---
+++ app_back/NicosApp/NicosApp.Infraestructura/Migrations/20210806234739_Agregando Cliente y estatus en la tabla usuarios.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NicosApp.Infraestructura.Migrations
+{
+    public partial class AgregandoClienteyestatusenlatablausuarios : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cliente",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Estatus",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cliente",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Estatus",
+                table: "AspNetUsers");
+        }
+    }
+}
